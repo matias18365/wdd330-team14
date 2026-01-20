d615b2986efa4588cc96b2d61fe3e1cad0b13b6c
gut23014@byui.edu <matias1805ga@gmail.com>
2026-01-19 23:45:00 -0400

    Completed the Individial Task

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { updateCartCount } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
@@ -8,3 +9,5 @@ const listElement = document.querySelector(".product-list");
 const productList = new ProductList("tents", dataSource, listElement);
 
 productList.init();
+
+updateCartCount();
