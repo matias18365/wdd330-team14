5fb28df53e4fb1192c9e78c955e6f67191185e5b
Samuel <awesamuelopeoluwa@gmail.com>
2026-01-26 16:07:01 +0100

    fix to quick view feature

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,36 @@ import ProductList from "./ProductList.mjs";
 import { updateCartCount } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
-
 const listElement = document.querySelector(".product-list");
 
 const productList = new ProductList("tents", dataSource, listElement);
-
 productList.init();
 
 updateCartCount();
+
+document.addEventListener("click", async (e) => {
+    console.log("document clicked", e.target);
+  const quickViewBtn = e.target.closest(".quick-view");
+  if (!quickViewBtn) return;
+  console.log(" Quick view clicked", quickViewBtn.dataset.id);
+
+  const id = quickViewBtn.dataset.id;
+  const product = await dataSource.findProductById(id);
+
+  document.getElementById("modalImage").src = product.Image;
+  document.getElementById("modalTitle").textContent =
+    `${product.Brand.Name} ${product.NameWithoutBrand}`;
+  document.getElementById("modalPrice").textContent =
+    `$${product.FinalPrice}`;
+  document.getElementById("modalDescription").innerHTML =
+    product.DescriptionHtmlSimple;
+
+  document.getElementById("quickViewModal")
+    .classList.remove("hidden");
+});
+
+
+document.getElementById("closeModal")?.addEventListener("click", () => {
+  document.getElementById("quickViewModal")
+    .classList.add("hidden");
+});
